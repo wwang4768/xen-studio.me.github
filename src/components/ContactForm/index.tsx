5a38b6930cbe3438530472506ace3e6d6e9eaeae
wwang4768 <wwang4768@gmail.com>
2024-10-16 18:16:17 -0700

    WIP emailJS implementation

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -9,6 +9,7 @@ import Block from "../Block";
 import Input from "../../common/Input";
 import TextArea from "../../common/TextArea";
 import { ContactContainer, FormGroup, Span, ButtonContainer } from "./styles";
+import emailjs from "emailjs-com";
 
 const Contact = ({ title, content, id, t }: ContactProps) => {
   const { values, errors, handleChange, handleSubmit } = useForm(validate);
@@ -17,6 +18,30 @@ const Contact = ({ title, content, id, t }: ContactProps) => {
     const ErrorMessage = errors[type as keyof typeof errors];
     return <Span>{ErrorMessage}</Span>;
   };
+
+  // Email sending function using EmailJS
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'YOUR_SERVICE_ID',
+        'YOUR_TEMPLATE_ID',
+        e.target as HTMLFormElement,
+        'YOUR_USER_ID'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("Message sent successfully!");
+        },
+        (error) => {
+          console.log(error.text);
+          alert("An error occurred, please try again.");
+        }
+      );
+  };
+
 
   return (
     <ContactContainer id={id}>
